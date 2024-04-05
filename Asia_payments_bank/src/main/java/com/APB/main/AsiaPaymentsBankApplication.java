82134d4d6452fdeeec5bf0dc90951311f96539e8
PENKEY123 <penkeymahesh.babu@shimakhtechnology.com>
2024-04-05 23:32:08 +0530

    first commit

---
+++ Asia_payments_bank/src/main/java/com/APB/main/AsiaPaymentsBankApplication.java
@@ -0,0 +1,13 @@
+package com.APB.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsiaPaymentsBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsiaPaymentsBankApplication.class, args);
+	}
+
+}
